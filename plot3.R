5f641a44a2e310eb115a63624dffe47b7d113b69
Victor Bucha <v.bucha@samsung.com>
2015-09-18 15:14:09 +0300

    plot3 is ready

---
+++ plot3.R
@@ -0,0 +1,16 @@
+### Loading the data##########
+NEI <- readRDS("summarySCC_PM25.rds")
+siSCC <- readRDS("Source_Classification_Code.rds")
+
+library(dplyr)
+library(ggplot2)
+
+#calculate total emissions per year for Baltimore City fips=="24510"
+Year_Emissions<-NEI %>% filter(fips==24510) %>% group_by(year,type) %>% summarise(Emissions=sum(Emissions/1000))
+
+#draw plot
+c<-ggplot(Year_Emissions,aes(x = factor(year), y = Emissions,fill=type))
+c + geom_bar(stat = "identity")+facet_wrap(~ type)+ggtitle("Baltimore City")+xlab("Year")+ylab("PM2.5 in thousands")
+
+dev.copy(png,"plot3.png")
+dev.off()
