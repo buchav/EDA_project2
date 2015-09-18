0bdd602c2246f6c545193eb0f9967b7304e993f5
Victor Bucha <v.bucha@samsung.com>
2015-09-18 12:09:32 +0300

    second

---
+++ plot1.R
@@ -0,0 +1,14 @@
+### Loading the data##########
+NEI <- readRDS("summarySCC_PM25.rds")
+siSCC <- readRDS("Source_Classification_Code.rds")
+
+library(dplyr)
+#calculate total emissions per year
+Year_Emissions<-NEI %>% group_by(year) %>% summarise(Emissions=sum(Emissions/1000))
+
+with (Year_Emissions, {
+        barplot(Emissions,col="blue",names.arg =year ,ylab="PM2.5 in thousands")
+        title (main="Total Emissions per Year",xlab="Year")
+        })
+
+#test
